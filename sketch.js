5ec4150aaede6a4b4ac62e5ef5b6858a9aedff7e
ShadowInferno123 <93428672+ShadowInferno123@users.noreply.github.com>
2021-11-06 14:18:22 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var ball1 
+function setup() {
+  createCanvas(600,600);
+  ball = createSprite(200,200,10,10);
+  ball.shapeColor = "purple"
+} 
+
+function draw() 
+
+
+{ 
+  background("black");
+  if (keyIsDown(RIGHT_ARROW)) {
+    background("red");
+    ball.position.x = ball.position.x +5  
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    ball.position.x = ball.position.x -5
+  }
+  if (keyIsDown(UP_ARROW)){
+    ball.position.y = ball.position.y -5
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    ball.position.y = ball.position.y +5
+  }
+  
+drawSprites();
+}
+
+
+
+
